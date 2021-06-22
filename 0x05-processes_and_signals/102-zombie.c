6d6b8e1f5c0573dbab9f29df28a675a6a1f93eb9
Diego Varela <2806@holbertonschool.com>
2021-06-22 15:01:18 +0000

    Upload task 102

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,15 +2,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+* infinite_while - Entry point
+*
+* Description: Make a infinite loop
+* Return: 0 on success
+*/
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
+/**
+* main - Entry point
+*
+* Description: Make 5 zombie process
+* Return: 0 on success, 1 on fail
+*/
 int main(void)
 {
 	pid_t pid0, pid1, pid2, pid3, pid4;
@@ -23,28 +35,28 @@ int main(void)
 
 	pid1 = fork();
 	if (pid1 > 0)
-                printf("Zombie process created, PID: %i\n", pid1);
-        else
-                exit(1);
+		printf("Zombie process created, PID: %i\n", pid1);
+	else
+		exit(1);
 
 	pid2 = fork();
 	if (pid2 > 0)
-                printf("Zombie process created, PID: %i\n", pid2);
-        else
-                exit(1);
+		printf("Zombie process created, PID: %i\n", pid2);
+	else
+		exit(1);
 
 	pid3 = fork();
 	if (pid3 > 0)
-                printf("Zombie process created, PID: %i\n", pid3);
-        else
-                exit(1);
+		printf("Zombie process created, PID: %i\n", pid3);
+	else
+		exit(1);
 
 	pid4 = fork();
 	if (pid4 > 0)
-                printf("Zombie process created, PID: %i\n", pid4);
-        else
-                exit(1);
-	
+		printf("Zombie process created, PID: %i\n", pid4);
+	else
+		exit(1);
+
 	infinite_while();
-	return 0;
+	return (0);
 }
