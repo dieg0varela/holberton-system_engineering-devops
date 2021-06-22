811a39bbe6fafd18e40f286bb253540cd23fb1b8
Diego Varela <2806@holbertonschool.com>
2021-06-22 14:47:56 +0000

    Upload task 102 WIP

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void)
+{
+	pid_t pid0, pid1, pid2, pid3, pid4;
+
+	pid0 = fork();
+	if (pid0 > 0)
+		printf("Zombie process created, PID: %i\n", pid0);
+	else
+		exit(1);
+
+	pid1 = fork();
+	if (pid1 > 0)
+                printf("Zombie process created, PID: %i\n", pid1);
+        else
+                exit(1);
+
+	pid2 = fork();
+	if (pid2 > 0)
+                printf("Zombie process created, PID: %i\n", pid2);
+        else
+                exit(1);
+
+	pid3 = fork();
+	if (pid3 > 0)
+                printf("Zombie process created, PID: %i\n", pid3);
+        else
+                exit(1);
+
+	pid4 = fork();
+	if (pid4 > 0)
+                printf("Zombie process created, PID: %i\n", pid4);
+        else
+                exit(1);
+	
+	infinite_while();
+	return 0;
+}
