ae3946affa526ae1eda8fdc1982d493177638b7c
Diego Varela <2806@holbertonschool.com>
2021-08-17 16:25:38 +0000

    Upload task 6

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# Create a holberton file in /tmp with specific permissions
+include stdlib
+
+package { 'nginx':
+    ensure          => 'present',
+    provider        => 'apt',
+    install_options => ['-y'],
+}
+
+file { '/var/www/html/index.html':
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'Holberton School',
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure => 'present',
+}->
+file_line {'Adding redirect':
+    path  => '/etc/nginx/sites-available/default',
+    after => 'server_name _;',
+    line  =>  '        rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
