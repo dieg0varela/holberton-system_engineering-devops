2e3a032867f9fa1ae6f7e79e0ba0664757f43d8a
Diego Varela <2806@holbertonschool.com>
2021-08-23 23:50:58 +0000

    Upload task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,44 @@
+# Create a holberton file in /tmp with specific permissions
+
+package { 'nginx':
+    ensure => present,
+}
+
+file { '/var/www/html/index.html':
+    ensure  => present,
+    content => 'Holberton School',
+}
+
+file { '/var/www/html/404.html':
+    ensure  => present,
+    content => "Ceci n'est pas une page",
+}
+
+file_line {'Adding_redirect':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'server_name _;',
+    line   =>  '        rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file_line {'Adding_404':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'server_name _;',
+    line   =>  '        error_page 404 /404.html;\n;',
+}
+
+file_line {'Adding_Header':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'server_name _;',
+    line   =>  '               add_header X-Served-By $HOSTNAME;\n;',
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['Adding_redirect','Adding_404', 'Adding_Header'],
+}
