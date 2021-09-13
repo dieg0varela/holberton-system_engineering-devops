d0aa53d6035ce5b00e30832d972b9c89f2456abb
Diego Varela <2806@holbertonschool.com>
2021-09-13 22:10:08 +0000

    Fix task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,21 +6,21 @@ import json
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     response = requests.get(url + "todos?userId=" + argv[1])
-    json = response.json()
-    userId = json[0].get('userId')
-    total_task = len(json)
+    jsonRes = response.json()
+    userId = jsonRes[0].get('userId')
+    total_task = len(jsonRes)
     user = requests.get(url + "users/" + str(userId)).json()
     uname = user.get('username')
     filename = str(userId) + ".json"
-    lista = []
-    for data in json:
-        dicti = {}
+    resu = {str(userId): []}
+    for data in jsonRes:
+        dicti = {
+            "task": "",
+            "completed": "",
+            "username": ""
+        }
         dicti['task'] = data.get('title')
         dicti['completed'] = data.get('completed')
         dicti['username'] = uname
-        lista.append(dicti)
-    with open(filename, "w") as f:
-        res = {}
-        res[str(userId)] = lista
-        f.write(str(res))
-    f.closed
+        resu[str(userId)].append(dicti)
+    print(resu)
