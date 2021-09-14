93b4a1d3984580f56ef9bc97da688c14c36d4989
Diego Varela <2806@holbertonschool.com>
2021-09-14 00:09:18 +0000

    Fix task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,27 +5,21 @@ import requests
 from sys import argv
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    response = requests.get(url + "todos")
+    response = requests.get(url + "users")
     jsonRes = response.json()
     filename = "todo_all_employees.json"
     resu = {}
-    dicti = {
-        "task": "",
-        "completed": "",
-        "username": ""
-    }
     for data in jsonRes:
-        userid = data.get('userId')
+        userid = data.get('id')
+        uname = data.get('username')
         resu[str(userid)] = []
-    for data in jsonRes:
-        userId = data.get('userId')
-        user = requests.get(url + "users/" + str(userId)).json()
-        uname = user.get('username')
-        dicti['task'] = data.get('title')
-        dicti['completed'] = data.get('completed')
-        dicti['username'] = uname
-        resu[str(userId)].append(dicti)
+        tasks = requests.get(url + "todos?userid=/" + str(userid)).json()
+        for task in tasks:
+            dicti = {}
+            dicti['task'] = task.get('title')
+            dicti['completed'] = task.get('completed')
+            dicti['username'] = uname
+            resu[str(userid)].append(dicti)
     with open(filename, "w") as f:
         f.write(json.dumps(resu))
     f.closed
-    print(resu)
