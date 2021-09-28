0dc4ed4c1f1cbe9a8f7c8edabed83c3cedccbb2a
Diego Varela <2806@holbertonschool.com>
2021-09-28 16:27:41 +0000

    Fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 '''Module to restrive number of subscribers of a subreddit'''
-
-
 import requests
+
 
 def number_of_subscribers(subreddit):
     '''Function to get data from reddit api'''
     url = "https://reddit.com/r/"
     headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0)\
                               Gecko/20100101 Firefox/15.0.1"}
-    resp = requests.get(url + subreddit + "/about.json", headers=headers)
-    if (resp.status_code == 404):
+    response = requests.get(url + subreddit + "/about.json",
+                            headers=headers)
+    try:
+        ret = response.json()['data'].get('subscribers')
+        return(ret)
+    except:
         return (0)
-    else:
-        print(resp.json()['data'].get('subscribers'))
