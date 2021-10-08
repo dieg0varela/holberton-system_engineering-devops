2aa1a161d195ffd3c8e05b627149619ac0e132e5
Diego Varela <2806@holbertonschool.com>
2021-10-08 01:10:58 +0000

    Upload files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+exec {'Correct Setting':
+    command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx",
+}
+exec { 'Restart Nginx':
+    require => Exec['Correct Setting'],
+    command => '/usr/bin/service nginx restart',
+}
